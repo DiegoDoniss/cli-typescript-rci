2a1f4b0ca971bc648c7388afdfbfba7471e14ceb
Diego Jared  Donis Soto <programacion4@recagua.com>
2022-01-29 21:45:22 -0600

    add const id and body in getbyid,putbyid, deletebyid

--- tscli.py
+++ tscli.py
@@ -17,18 +17,52 @@ def new(file_name, crud):
         crudController = """
     async getAll(req: Request, res: Response){
         const pool = await getPool()
+        try {
+            const result = await pool.query('')
+            res.send(result.recordset)
+        } catch (error) {
+            res.status(404).send({message:'error en la consulta'})
+        }
     }
     async getById(req: Request, res: Response){
         const pool = await getPool()
+        const { id } = req.params
+        try {
+            const request = pool.request()
+            request.input('id',Int,id)
+        } catch (error) {
+            res.status(404).send({message:'error en la consulta'})
+        }
     }
     async create(req: Request, res: Response){
         const pool = await getPool()
+        const body = req.body
+        try {
+            const request = pool.request()
+            request.input()
+        } catch (error) {
+            res.status(404).send({message:'error en la consulta'})
+        }
     }
     async editById(req: Request, res: Response){
         const pool = await getPool()
+        const { id } = req.params
+        try {
+            const request = pool.request()
+            request.input('id',Int,id)
+        } catch (error) {
+            res.status(404).send({message:'error en la consulta'})
+        }
     }
     async deleteById(req: Request, res: Response){
         const pool = await getPool()
+        const { id } = req.params
+        try {
+            const request = pool.request()
+            request.input('id',Int,id)
+        } catch (error) {
+            res.status(404).send({message:'error en la consulta'})
+        }
     }
 """
         crudRouter = """
@@ -36,7 +70,7 @@ router.get('/',controller.getAll)
 router.get('/:id',controller.getById)
 router.post('/',controller.create)
 router.put('/:id',controller.editById)
-router.delete('/',controller.deleteById)
+router.delete('/:id',controller.deleteById)
 """
 
     files = [
@@ -44,6 +78,7 @@ router.delete('/',controller.deleteById)
             "route": f'src/controllers/controller-{file_name}.ts',
             "content": f"""import {{ Request, Response }} from 'express'
 import {{ getPool }} from '../database'
+import {{ Int }} from 'mssql'
 export class {file_name.title()} {{{crudController}}}"""},
         {
             "route": f'src/interfaces/interface-{file_name}.ts',
